49873daaf5c825257c32afead17a442bcd3311c2
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2017-05-11 11:41:45 +0300

    Revert "Added locations page"
    This reverts commit 5083fff3c729a1aa128aa85d0dddfe7e225ae25f.

--- EnvironmentDashboardModule4/src/app/app.component.ts
+++ EnvironmentDashboardModule4/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { LocationsComponent } from './locations/locations.component';
-import {LocationsService} from './locations.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [LocationsService]
-  })
+  styleUrls: ['./app.component.css']
+})
 export class AppComponent {
   title = 'app works!';
 }
